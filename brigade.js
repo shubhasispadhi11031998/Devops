f29a4be12c22c65709048a932b1ee0e1c707751d
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-01-22 23:15:42 +0530

    new modified

--- brigade.js
+++ brigade.js
@@ -2,8 +2,8 @@ const { events, Job } = require("brigadier");
 events.on("exec", () => {
   var job = new Job("do-nothing", "alpine:3.8");
   job.tasks = [
-    "echo Hello",
-    "echo World"
+    "echo Good",
+    "echo Bye"
   ];
 
   job.run();
