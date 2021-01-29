87469060e299298df51ba7179a9962380dcce803
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-01-29 15:35:54 +0530

    modified

--- brigade.js
+++ brigade.js
@@ -1,5 +1,5 @@
 const { events, Job } = require("brigadier");
-events.on("push", async () => {
+events.on("exec", async (e, project) => {
   var job = new Job("my-firstjob", "amitsanu/brigimagef:latest");
   job.priviliged = true;
   let keyval = {
