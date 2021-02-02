1849e7461c53a965de815743dfafd18180c5a915
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-02-02 16:28:01 +0530

    modified

--- brigade.js
+++ brigade.js
@@ -62,7 +62,6 @@ events.on("push", async (e, project) => {
     "gcloud auth configure-docker",
     "docker version",
     "docker images",
-    "docker pull apline",
     "docker build -t helloworld:latest gcr.io/vocal-raceway-299310/hello-world:v1",
     "docker push gcr.io/vocal-raceway-299310/hello-world:v1",
     "echo docker image pushed"
