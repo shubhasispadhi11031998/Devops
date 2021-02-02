8fe69f568323e1717da0829df78a27f53ebf6770
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-02-02 12:50:48 +0530

    modified

--- brigade.js
+++ brigade.js
@@ -32,19 +32,6 @@ events.on("push", async (e, project) => {
   ]
   const jobs = new Job("my-helm","amitsanu/brigadeimage1:latest");
     jobs.privileged = true;
-    let keyval = {
-      type: project.secrets.type,
-      project_id: project.secrets.project_id,
-      private_key_id: project.secrets.private_key_id,
-      private_key: project.secrets.private_key,
-      client_email: project.secrets.client_email,
-      client_id: project.secrets.client_id,
-      auth_uri: project.secrets.auth_uri,
-      token_uri: project.secrets.token_uri,
-      auth_provider_x509_cert_url: project.secrets.auth_provider_x509_cert_url,
-      client_x509_cert_url: project.secrets.client_x509_cert_url,
-      };
-      let keyvalobj = JSON.stringify(keyval);
     jobs.env = {
       DOCKER_DRIVER: "overlay"
       };
