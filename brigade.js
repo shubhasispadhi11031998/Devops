dd01f0f15eac093ef9d9fe63ced28b8a982a3113
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-02-03 12:25:27 +0530

    modified

--- brigade.js
+++ brigade.js
@@ -43,17 +43,25 @@ events.on("push", async (e, project) => {
     
     
     // docker image pushing to gcp
-    "dockerd &",
-    "dockerd-entrypoint.sh &",
-    "gcloud auth configure-docker",
-    "docker version",
-    "cd /src",
-    "ls",
-    "docker build -t mydocker:latest .",
-    "docker tag mydocker:latest gcr.io/vocal-raceway-299310/mydocker:v1",
-    "docker push gcr.io/vocal-raceway-299310/mydocker:v1",
-    "echo docker image pushed"
-  ];
+    // "dockerd &",
+    // "dockerd-entrypoint.sh &",
+    // "gcloud auth configure-docker",
+    // "docker version",
+    // "cd /src",
+    // "ls",
+    // "docker build -t mydocker:latest .",
+    // "docker tag mydocker:latest gcr.io/vocal-raceway-299310/mydocker:v1",
+    // "docker push gcr.io/vocal-raceway-299310/mydocker:v1",
+    // "echo docker image pushed"
+  ]
 
+  const lint = new Job("my-lint","node:alpine");
+  lint.tasks = [
+    "cd /src",
+    "npm i",
+    "npm run lint",
+    "echo lint done successfully"
+  ];
   job.run();
+  lint.run();
 });
