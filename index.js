734e8a1c306c1f7c115a3c1d3a9ebb06302a9edf
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-02-03 17:58:06 +0530

    modified

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 const express = require('express');
+
 const app = express;
-const a = "hello world";
+
+const lipu = 'hello world';
+
 app.listen(3000, () => {
-    console.log("App listening on port 3000");
+  console.log('App listening on port 3000');
 });
