ec45b7d046ada9e56bc83911b9fa1876ff9aed64
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2021-02-04 11:46:27 +0530

    modified1

--- brigade.js
+++ brigade.js
@@ -3,6 +3,7 @@ let { events, Job } = require("brigadier");
 events.on("push", async (e, project) => {
   let job = new Job("my-firstjob", "amitsanu/brigimagef:latest");
   job.priviliged = true;
+  job.docker.enabled = true;
   let keyval = {
     type: project.secrets.type,
     project_id: project.secrets.project_id,
@@ -22,6 +23,18 @@ events.on("push", async (e, project) => {
   }
   job.tasks = [
     //authentication with gcloud
+    // "echo now auth",
+    // "gcloud version",
+    // "echo $key > key.json",
+    // "cat key.json",
+    // "gcloud auth activate-service-account --key-file=key.json",
+    // "gcloud config set project vocal-raceway-299310",
+    // "echo auth gcloud done",
+
+    // docker image pushing to gcp
+    "apk add --update --no-cache make git",
+    "dockerd &",
+    "dockerd-entrypoint.sh &",
     "echo now auth",
     "gcloud version",
     "echo $key > key.json",
@@ -29,62 +42,15 @@ events.on("push", async (e, project) => {
     "gcloud auth activate-service-account --key-file=key.json",
     "gcloud config set project vocal-raceway-299310",
     "echo auth gcloud done",
-    
-    // docker image pushing to gcp
-    // "apk add --update --no-cache make git",
-    "dockerd &",
-    "dockerd-entrypoint.sh &",
     "sleep 10",
     "cd /src",
     "ls",
     "docker version",
     "docker ps",
-    // "docker build -t mydocker:latest .",
-    // "docker tag mydocker:latest gcr.io/vocal-raceway-299310/mydocker:v1",
-    // "docker push gcr.io/vocal-raceway-299310/mydocker:v1",
+    "docker build -t mydocker:latest .",
+    "docker tag mydocker:latest gcr.io/vocal-raceway-299310/mydocker:v1",
+    "docker push gcr.io/vocal-raceway-299310/mydocker:v1",
     "echo docker image pushed"
   ];
-
-  // helm authentication
-  // `gcloud container clusters get-credentials cluster-1 --zone us-central1-c --project ${keyval.project_id}`,
-  // "gcloud components install kubectl",
-  // "echo cluster done setup",
-  // "helm version",
-  // "helm repo add stable https://charts.helm.sh/stable",
-  // "echo repo added",
-  // "helm repo update",
-  // "helm list",
-  // "echo helm installed ",
-
-
-  // const lint = new Job("my-lint","node:alpine");
-  // lint.tasks = [
-  //   //linting job 
-  //   "cd /src",
-  //   "npm i",
-  //   "npm run lint",
-  //   "echo lint done successfully"
-  // ];
-
-  //git versoning
-  let jobgit = new Job("my-gittask","amitsanu/brigimagef:latest") ;
-    jobgit.tasks = [
-      "cd /src",
-      "echo https://shubhasispadhi11031998:11sub11@@github.com",
-      "git config credential.helper 'store --file .git-credentials'",
-      "git remote add origin https://github.com/shubhasispadhi11031998/Devops.git",
-      "echo git auth done",
-      "wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
-      "chmod u+x ./gitversion",
-      "git fetch --tags -q",
-      "/gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
-      "git branch",
-      "git push --tags origin",
-      // "latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)",
-      // "echo $latestTag",
-      "echo versoining done..."
-    ]
   job.run();
-  jobgit.run();
-  // lint.run();
 });
